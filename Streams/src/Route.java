106bfba287f18a981516039950ea8cf720c8e1f0
Rafael Guterres Jeffman <rgjeffman@senacrs.com.br>
2016-08-01 20:12:19 -0300

    Criada a classe de rotas. Para abstrair objetos do arquivo.

---
+++ Streams/src/Route.java
@@ -0,0 +1,72 @@
+
+public class Route {
+	
+	private String id;
+	private String agencyId; // TODO to remove
+	private String shortName;
+	private String longName;
+	private String desc;
+	private String type;
+	private String url;
+	private String color;
+	private String textColor;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAgencyId() {
+		return agencyId;
+	}
+	public void setAgencyId(String agencyId) {
+		this.agencyId = agencyId;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	public String getLongName() {
+		return longName;
+	}
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTextColor() {
+		return textColor;
+	}
+	public void setTextColor(String textColor) {
+		this.textColor = textColor;
+	}
+	
+	
+	
+	
+}
