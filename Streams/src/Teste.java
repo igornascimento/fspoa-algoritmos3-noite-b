5f8381bab8764904ba3b04f6347cc6f8d0c4f869
Rafael Guterres Jeffman <rgjeffman@senacrs.com.br>
2016-08-08 20:44:14 -0300

    Removidas linhas em branco para melhor projecao do codigo.

--- Streams/src/Teste.java
+++ Streams/src/Teste.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 
 public class Teste {
 
-	
 	public static void main(String[] args) {
 		
+		String filename = "data/routes.txt";
 		
-		try {
-		
-			CSVFileReader csvArquivo = new CSVFileReader("data/routes.txt");
-			
+		try {		
+			CSVFileReader csvArquivo = new CSVFileReader(filename);
 			while(csvArquivo.hasNext()){
 				csvArquivo.next();
-			}	
-			
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
 }
