5d3952334b12403a2a120812b2ea7c58bf2ee970
Rafael Guterres Jeffman <rafasgj@gmail.com>
2016-09-20 00:12:08 -0300

    Modificado o codigo para suportar double hashing.

--- TabelasHash/src/HashTable.java
+++ TabelasHash/src/HashTable.java
@@ -1,6 +1,6 @@
 
 interface Probing {
-	int probe(int i, int j);
+	int probe(Object key, int hash, int j, int size);
 }
 
 class ChaveValor {
@@ -18,10 +18,28 @@ public class HashTable {
 	private Probing probe;
 
 	public static void main(String[] args) {
+		/*
 		// linear: (i + j) % B
-		//HashTable hashTable = new HashTable((i,j)->(i+j));
-		// quadratico: (i + 2**j) % B
-		HashTable hashTable = new HashTable((i,j)->(i+(1<<j)));
+		HashTable hashTable = new HashTable(
+			(k,h,j,B)-> {
+				int h2;
+				return (h2 = ((h+j)% B))>0?h2:-1*h2;
+			});
+		*/
+		/*
+		// quadratico: (h + 2**j) % B
+		HashTable hashTable = new HashTable(
+			(k,h,j,B)-> {
+				int h2;
+				return (h2 = (h+(1<<j))% B)>0?h2:-1*h2;
+			});
+		*/
+		// double hash: (h1 + h2*j) % B
+		HashTable hashTable = new HashTable(
+			(Object k, int h, int j, int B) -> {
+				int h2 = String.format("%s%d",k.toString(),h).hashCode();
+				return (h2 = (h+h2*j)% B) > 0 ? h2 : -1*h2; 
+			});
 
 		hashTable.put("123","Rafael");
 		hashTable.put("124","Ivonei");
@@ -41,14 +59,12 @@ public class HashTable {
 	public HashTable(Probing probe) {
 		this.probe = probe;
 	}
-	
+
 	private String get(String key)
 	{
 		int hk = hash(key);
-		int i = hk % tabela.length;
-		// probing quadratico
 		for (int j = 0; j < tabela.length; j++) {
-			int indice = probe.probe(i, j) % tabela.length;
+			int indice = probe.probe(key, hk, j, tabela.length);
 			if (tabela[indice] == null) {
 				throw new KeyNotFoundException(key);
 			} else {
@@ -64,10 +80,9 @@ public class HashTable {
 	private void put(String key, String value) {
 		System.out.println("Inserindo: (" + key+","+value+")");
 		int hk = hash(key);
-		int i = hk % tabela.length;
 		// probing linear
 		for (int j = 0; j < tabela.length; j++) {
-			int indice = probe.probe(i, j) % tabela.length;
+			int indice = probe.probe(key, hk, j, tabela.length);
 			System.out.print("Tentando: " + indice + "... ");
 			if (tabela[indice] == null) {
 				System.out.println("ok.");
